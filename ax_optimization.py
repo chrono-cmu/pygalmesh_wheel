945b0984fa5245013c3cd6f06c2296b51d83f387
LanceMatthies <lmatthie@andrew.cmu.edu>
2025-07-06 01:47:12 -0400

    test multiple slip values

--- ax_optimization.py
+++ ax_optimization.py
@@ -10,8 +10,6 @@ import json
 import subprocess
 import gmail_monitor
 import pandas as pd
-
-
 def evaluate(parameters):
         rim_radius = parameters["rim_radius"]
         width = parameters["width"]
@@ -21,7 +19,6 @@ def evaluate(parameters):
         control_point_deviation = parameters["control_point_deviation"]
         wave_number = parameters["wave_number"]
         wave_amplitude = parameters["wave_amplitude"]
-        
         score = 0
         score += rim_radius * 0.6
         score += width * 0.4
@@ -34,17 +31,14 @@ def evaluate(parameters):
         noise_scale = random.random()*.4 +.8
         score *= noise_scale
         return score
-
-
 client = Client()
-
 parameters = [
     RangeParameterConfig(
         name="rim_radius", parameter_type="float", bounds=(0, 1)
     ),
     RangeParameterConfig(
         name="width", parameter_type="float", bounds=(0, 1)
-    ),    
+    ),
     RangeParameterConfig(
         name="grouser_number", parameter_type="float", bounds=(0, 1)
     ),
@@ -64,24 +58,17 @@ parameters = [
         name="wave_amplitude", parameter_type="float", bounds=(0, 1)
     )
 ]
-
 client.configure_experiment(parameters=parameters)
-test_arrays = ["32720007", "32781190", "33283836", "33283810", "32461246", "32724206", "32718125", "32718537", "32721460"]
 metric_name = "score" # this name is used during the optimization loop in Step 5
 objective = f"{metric_name}" # minimization is specified by the negative sign
-rim_radius = parameters["rim_radius"]
 client.configure_optimization(objective=objective)
-
-
 for batch_number in range(3): # Run 10 rounds of trials
     # We will request three trials at a time in this example
     trials = client.get_next_trials(max_trials=3)
-
     #get batch parameters
     job_array = []
     for trial_index, parameters in trials.items():
         job_array.append([trial_index, parameters, 0])
-
     #launch every job
     for i in range(len(job_array)):
         trial_index, parameters, _ = job_array[i]
@@ -99,7 +86,6 @@ for batch_number in range(3): # Run 10 rounds of trials
         # download wheel json
         with open("wheel_jsons/wheel_parameters.json", "w") as json_file:
             json.dump(wheel_param, json_file, indent=4)
-
         # create json w/ job parameters
         job_json = {
             "terrain_filepath": "/jet/home/matthies/moonranger_mobility/terrain/GRC_3e5_Reduced_Footprint.csv",
@@ -112,45 +98,41 @@ for batch_number in range(3): # Run 10 rounds of trials
         # download job json
         with open("job_json/job_parameters.json", "w") as json_file:
             json.dump(job_json, json_file, indent=4)
-
         # bash script creates wheel mesh, uploads mesh & json, and launches script
-        # print("start bash script")
-        # script_result = subprocess.run(["./automated_pipeline.sh", "upload", str(trial_index)], capture_output=True, text=True)
-        # JOB_ID = 0
-        # if script_result.returncode == 0:
-        #     output_lines = script_result.stdout.strip().split('\n')
-        #     for line in output_lines:
-        #         if "job id:" in line:
-        #             JOB_ID = line.split(":")[1]
-        #     if JOB_ID == 0:
-        #         print("job id not found")
-        #     else:
-        #         print("Script success and jobid captured "+ JOB_ID)
-        # else:
-        #     print("Script failed or filename not capture")
-        # print("end bash script")
-        # print("stdout:")
-        # print(script_result.stdout)
-        # print("stderr:")
-        # print(script_result.stderr)
-
-        job_array[i][2] = test_arrays[trial_index]
-
-
+        print("start bash script")
+        script_result = subprocess.run(["./automated_pipeline.sh", "upload", str(trial_index)], capture_output=True, text=True)
+        JOB_ID = 0
+        if script_result.returncode == 0:
+            output_lines = script_result.stdout.strip().split('\n')
+            for line in output_lines:
+                if "job id:" in line:
+                    JOB_ID = line.split(":")[1]
+            if JOB_ID == 0:
+                print("job id not found")
+            else:
+                print("Script success and jobid captured "+ JOB_ID)
+        else:
+            print("Script failed or filename not capture")
+        print("end bash script")
+        print("stdout:")
+        print(script_result.stdout)
+        print("stderr:")
+        print(script_result.stderr)
+        job_array[i] = [trial_index, parameters, JOB_ID]
     print("start gmail monitor")
     job_ids = [row[2] for row in job_array]
     gmail_monitor.monitor(job_ids)
     # slip values currently tested
     slip_values = [-0.1, 0.0, 0.1, 0.3]
     for trial_index, parameters, job_id in job_array:
-
+        rim_radius = parameters["rim_radius"]
         # average drawbar pulls for each slip
         avg_dcs = []
         z_variances = []
         sinkages = []
         for slip in slip_values:
-            df = pd.read_csv('~/Documents/wheel_sim_data/automation_test_' 
-                             + job_id + '/SkidSteerSim_' + slip + 
+            df = pd.read_csv('~/Documents/wheel_sim_data/automation_test_'
+                             + job_id + '/SkidSteerSim_' + slip +
                              '/output.csv')
             avg_dcs.append((df["f_x"] / df["f_z"]).mean())
             z_variances.append(df["pos_z"].var(ddof=0)) # leaving blank does unbiased variance
@@ -175,7 +157,6 @@ for batch_number in range(3): # Run 10 rounds of trials
                 point1 = p1
                 point2 = p2
                 break
-
         if(slip1 is None):
             print(f"no 0 intercept found")
         else:
@@ -186,7 +167,6 @@ for batch_number in range(3): # Run 10 rounds of trials
         slips_shifted = [s - x_inter for s in slip_values]
         #area under curve of zero crossing
         dcs_under_curve = np.trapz(avg_dcs, slips_shifted)
-
         # MASS
         mesh_file = "~/Documents/wheel_sim_pipeline/wheel.obj"
         # run Blender headless to compute mass
@@ -196,10 +176,8 @@ for batch_number in range(3): # Run 10 rounds of trials
             "--python", "compute_volume.py",
             "--", mesh_file, "2.7"           # density = 2.7 g/cm³
         ], capture_output=True, text=True)
-
         if proc.returncode != 0:
             raise RuntimeError(f"Volume calc failed:\n{proc.stderr}")
-
         mass = float(proc.stdout.strip())
         z_var = float(np.mean(z_variances))
         sinkage = float(np.mean(sinkages)) - rim_radius - 0.41
@@ -207,34 +185,24 @@ for batch_number in range(3): # Run 10 rounds of trials
         score = dcs_under_curve * 0.75 + mass * 0.1 + z_var * 0.05 + 0.1 * sinkage
         # Set raw_data as a dictionary with metric names as keys and results as values
         raw_data = {metric_name: score}
-
         # Complete the trial with the result
         client.complete_trial(trial_index=trial_index, raw_data=raw_data)
-
         print(f"Completed trial {trial_index} with {raw_data=}")
-
-        
-
 best_parameters, prediction, index, name = client.get_best_parameterization()
 print("Best Parameters:", best_parameters)
 print("Prediction (mean, variance):", prediction)
-
 # display=True instructs Ax to sort then render the resulting analyses
 # cards = client.compute_analyses(display=False)
 # print(cards)
 # for card in cards:
 #      print(card)
-
 # saving the dataframe
 summary = client.summarize()
 summary.to_csv("data.csv", index=False)
 print(type(summary))
-
 x = summary["rim_radius"]
 y = summary["score"]
-
 plt.scatter(x, y, c="blue", alpha=0.3, marker="o", zorder=3000)
-
 plt.xlabel("rim_radius")
 plt.ylabel("Score")
 plt.title("Simulation Evaluation")
